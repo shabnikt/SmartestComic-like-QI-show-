0c61a7d3e8ba2043e05fc644259b50190644007f
mykyta.shabanov <mykyta.shabanov@smartify-it.de>
2022-11-04 02:48:39 +0100

    Restructure front

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import customtkinter
+import tkinter
 from func_math import *
 from smartlog import log
+import os
+import json
 
 log.setLevel('DEBUG')
-
-customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-# app.geometry('800x400')
-app.attributes("-fullscreen", True)
 
 
 def table_movement(players_list):
@@ -51,47 +47,52 @@ def upp_score(player):
     table_movement(players_list)
 
 
+# MAIN APP
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+# app.geometry('800x400')
+app.attributes("-fullscreen", True)
+
+
 # TABLE FRAMES
-score_frame = customtkinter.CTkFrame(master=app, fg_color='#212325', corner_radius=10)
+score_frame = customtkinter.CTkFrame(master=app, fg_color="#212325", corner_radius=10)
 score_frame.place(rely=0.5, relwidth=1, relheight=0.5)
 
-frames_dict = {'master': score_frame, 'corner_radius': 10}
+frames_dict = {"master": score_frame, "corner_radius": 10}
 player1 = customtkinter.CTkFrame(**frames_dict)
 player2 = customtkinter.CTkFrame(**frames_dict)
 player3 = customtkinter.CTkFrame(**frames_dict)
 player4 = customtkinter.CTkFrame(**frames_dict)
 
-place_dict = {'relx': 0.5, "relwidth": 0.8, "relheight": 0.2, "anchor": customtkinter.CENTER}
+score_dict = {player1: 0, player2: 0, player3: 0, player4: 0}
+frames_list = list(score_dict.keys())
+
+place_dict = {"relx": 0.5, "relwidth": 0.8, "relheight": 0.2, "anchor": customtkinter.CENTER}
 rely_list = get_rely_list(place_dict["relheight"])
 
-score_dict = {player1: 0, player2: 0, player3: 0, player4: 0}
-s = dict(sorted(score_dict.items(), key=lambda item: item[1], reverse=True))
-l = list(s.keys()).index(player1)
+for i in range(len(frames_list)):
+    frames_list[i].place(**place_dict, rely=rely_list[i])
 
-
-player1.place(**place_dict, rely=rely_list[0])
-player2.place(**place_dict, rely=rely_list[1])
-player3.place(**place_dict, rely=rely_list[2])
-player4.place(**place_dict, rely=rely_list[3])
-
-# log.debug(player1.place_info()['rely'])
-# log.debug(player2.place_info()['rely'])
-# log.debug(player3.place_info()['rely'])
-# log.debug(player4.place_info()['rely'])
-
-
-# Buttons
-butt_dict = {"text": "CTkButton"}  # , "command": lambda: player_movement(0.14)
-b1 = customtkinter.CTkButton(master=player1, **butt_dict, command=lambda: upp_score(player1))
-b2 = customtkinter.CTkButton(master=player2, **butt_dict, command=lambda: upp_score(player2))
-b3 = customtkinter.CTkButton(master=player3, **butt_dict, command=lambda: upp_score(player3))
-b4 = customtkinter.CTkButton(master=player4, **butt_dict, command=lambda: upp_score(player4))
-
+# BUTTONS
+butt_dict = {"text": "CTkButton"}
 bplace_dict = {"relx": 0.5, "rely": 0.5, "anchor": customtkinter.CENTER}
-b1.place(**bplace_dict)
-b2.place(**bplace_dict)
-b3.place(**bplace_dict)
-b4.place(**bplace_dict)
+
+buttons = [customtkinter.CTkButton(master=frames_list[i], **butt_dict, command=lambda i=i: upp_score(frames_list[i]))
+           for i in range(4)]
+for button in buttons:
+    button.place(**bplace_dict)
+
+# LABELS
+names = [os.getenv('NAME1'), os.getenv('NAME2'), os.getenv('NAME3'), os.getenv('NAME4')]
+names_dict = {"width": 120, "height": 50, "fg_color": "#2a2d2e", "text_font": ('Arial', 30), "corner_radius": 8}
+lplace_dict = {"relx": 0.1, "rely": 0.5, "anchor": customtkinter.CENTER}
+
+labels = [customtkinter.CTkLabel(master=frames_list[i], text=names[i], **names_dict) for i in range(4)]
+for label in labels:
+    label.place(**lplace_dict)
+# TODO SCORE LABELS
 
 app.mainloop()
 
@@ -99,6 +100,10 @@ app.mainloop()
 #     pass
 # https://www.youtube.com/watch?v=6NSjTE--hjw&ab_channel=DJDaveHarris-Topic
 # player1.winfo_rooty()
+
+# with open("config.json", "r", encoding='utf-8') as json_mapping:
+#     config = json.load(json_mapping)
+
 
 # def player_movement(finish):
 #     rely = float(player1.place_info()['rely'])
