1d57941d6368fd5fb869b429847fb20d8228acd8
mykyta.shabanov <wwnikitaww@gmail.com>
2022-11-27 22:31:30 +0100

    Fix cat anim

--- src/help_lib/categories_animation.py
+++ src/help_lib/categories_animation.py
@@ -22,7 +22,7 @@ def animate_categories(app, score_frame, frame, cat_dict):
 
 
 def create_top_level(anim_frame, cat_dict):
-    transparent_color = '#b71111'
+    transparent_color = '#fcfcfc'
     top = customtkinter.CTkToplevel(anim_frame, fg_color=transparent_color)
     top.attributes("-fullscreen", True)
     top.wm_attributes("-transparentcolor", transparent_color)
@@ -31,7 +31,7 @@ def create_top_level(anim_frame, cat_dict):
     font_size = 2
 
     label = customtkinter.CTkLabel(master=top, text='Выбирает категорию\nНИКИТА', text_font=('Helvetica', font_size),
-                                   text_color='#c90f0f', fg_color=transparent_color, bg_color=transparent_color)
+                                   text_color='#ffffff', fg_color=transparent_color, bg_color=transparent_color)
     label.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
     resize_label(label, font_size, anim_frame, cat_dict, top)
@@ -69,58 +69,66 @@ def themes_loop(n, anim_frame, categories, cat_dict, top):
     n = n if n < len(categories) else 0
 
     curr_width = 0.3
-    blackx = 0.4
-    blacky = (1920 * (blackx - curr_width) / 1080) + curr_width
 
     sizex = int(1920 * curr_width)
     sizey = int(1080 * curr_width)
+    print(f'{sizex}x{sizey}')
 
     img = get_img(cat_dict[categories[n]][0], sizex, sizey)
 
-    border = tkinter.Label(master=anim_frame, background='red')
-    border.place(relx=0.5, rely=0.5, relwidth=blackx, relheight=blacky, anchor=customtkinter.CENTER)
-
-    theme = tkinter.Label(master=anim_frame, background='#212325', image=img)
+    theme = tkinter.Label(master=anim_frame, background='black', image=img)
     theme.image = img
     theme.place(relx=0.5, rely=0.5, relwidth=curr_width, relheight=curr_width, anchor=customtkinter.CENTER)
 
     theme.bind("<Button-1>", lambda x: save_choose(categories[n]))
 
-    resize_theme(n, anim_frame, theme, border, categories, cat_dict, top)
+    resize_theme(n, anim_frame, theme, categories, cat_dict, top)
 
 
-def resize_theme(n, anim_frame, theme, border, categories, cat_dict, top):
-    if theme.place_info()['relwidth'] < '1':
-        curr_width = float(theme.place_info()['relwidth']) + 0.1
-        border_width = float(border.place_info()['relwidth'])
+def resize_theme(n, anim_frame, theme, categories, cat_dict, top):
+    while theme.place_info()['relwidth'] < '1.1':
+        prev_width = float(theme.place_info()['relwidth'])
+        if prev_width >= 1:
+            step = 0.05
+            height = 0.08
+        elif prev_width >= 0.9:
+            step = 0.1
+            height = 0.06
+        elif prev_width >= 0.8:
+            step = 0.1
+            height = 0.04
+        elif prev_width >= 0.6:
+            step = 0.1
+            height = 0
+        else:
+            step = 0.2
+            height = 0
 
-        blackx = border_width + 0.1 if border_width < 1 else 1
-        blacky = (1920 * (blackx - curr_width) / 1080) + curr_width
+        curr_width = prev_width + step
 
         sizex = int(1920 * curr_width)
-        sizey = int(1080 * curr_width)
+        sizey = int(1080 * (curr_width + height))
+        print(f'{sizex}x{sizey}')
 
         img = get_img(cat_dict[categories[n]][0], sizex, sizey)
-
-        border.place(relx=0.5, rely=0.5, relwidth=blackx, relheight=blacky, anchor=customtkinter.CENTER)
 
         theme.place(relx=0.5, rely=0.5, relwidth=curr_width, relheight=curr_width, anchor=customtkinter.CENTER)
         theme.configure(image=img)
         theme.image = img
 
-        theme.after(1000, resize_theme, n, anim_frame, theme, border, categories, cat_dict, top)
+        anim_frame.update()
+
     else:
         if exists('theme.json'):
             theme_last_turn(n, anim_frame, categories, cat_dict, top)
         else:
             n += 1
+            print()
             themes_loop(n, anim_frame, categories, cat_dict, top)
 
 
 def theme_last_turn(n, anim_frame, categories, cat_dict, top):
-    curr_width = 0.3
-    blackx = 0.4
-    blacky = (1920 * (blackx - curr_width) / 1080) + curr_width
+    curr_width = 0.4
 
     sizex = int(1920 * curr_width)
     sizey = int(1080 * curr_width)
@@ -128,36 +136,36 @@ def theme_last_turn(n, anim_frame, categories, cat_dict, top):
     path = cat_dict[categories[n]][1]
     img = get_img(path, sizex, sizey)
 
-    border = tkinter.Label(master=anim_frame, background='red')
-    border.place(relx=0.5, rely=0.5, relwidth=blackx, relheight=blacky, anchor=customtkinter.CENTER)
-
     theme = tkinter.Label(master=anim_frame, background='#212325', image=img)
     theme.image = img
     theme.place(relx=0.5, rely=0.5, relwidth=curr_width, relheight=curr_width, anchor=customtkinter.CENTER)
 
-    last_resize(n, anim_frame, theme, border, path, top)
+    last_resize(n, anim_frame, theme, path, top)
 
 
-def last_resize(n, anim_frame, theme, border, path, top):
-    if theme.place_info()['relwidth'] < '1':
-        curr_width = float(theme.place_info()['relwidth']) + 0.1
-        border_width = float(border.place_info()['relwidth'])
+def last_resize(n, anim_frame, theme, path, top):
+    while theme.place_info()['relwidth'] < '1':
+        prev_width = float(theme.place_info()['relwidth'])
+        if prev_width in (0.6, 0.7, 0.8):
+            step = 0.1
+        elif prev_width >= 0.9:
+            step = 0.025
+        else:
+            step = 0.2
 
-        blackx = border_width + 0.1 if border_width < 1 else 1
-        blacky = (1920 * (blackx - curr_width) / 1080) + curr_width
+        curr_width = prev_width + step
 
         sizex = int(1920 * curr_width)
         sizey = int(1080 * curr_width)
 
         img = get_img(path, sizex, sizey)
 
-        border.place(relx=0.5, rely=0.5, relwidth=blackx, relheight=blacky, anchor=customtkinter.CENTER)
-
         theme.place(relx=0.5, rely=0.5, relwidth=curr_width, relheight=curr_width, anchor=customtkinter.CENTER)
         theme.configure(image=img)
         theme.image = img
 
-        theme.after(1000, last_resize, n, anim_frame, theme, border, path, top)
+        anim_frame.update()
+
     else:
         end_animation(anim_frame, top)
 
